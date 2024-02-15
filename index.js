3569b97d8153e04228dedbb0c531bd6bc9f3e5e8
IbrahimHadidy <108360371+IbrahemHadidy@users.noreply.github.com>
2024-02-15 18:45:40 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+// index.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", (req, res) => {
+  res.json({greeting: 'hello API'});
+});
+
+// Define route handler for /api/:date?
+app.get("/api/:date?", (req, res) => {
+  // Get the date parameter from the request
+  const dateParam = req.params.date;
+
+  // Check if dateParam exists and if it's a valid date
+  if (!dateParam) {
+    // If dateParam is empty, return the current time
+    const now = new Date();
+    res.json({ unix: now.getTime(), utc: now.toUTCString() });
+    return;
+  }
+
+  const inputDate = new Date(dateParam);
+
+  // Check if the inputDate is valid
+  if (!isNaN(inputDate.getTime())) {
+    // Create Unix timestamp
+    const unixTimestamp = inputDate.getTime();
+
+    // Create UTC string
+    const utcString = inputDate.toUTCString();
+
+    // Return the response
+    res.json({ unix: unixTimestamp, utc: utcString });
+    return;
+  }
+
+  // If date is invalid, return an error message
+  res.status(400).json({ error: "Invalid Date" });
+});
+
+
+// Listen on port set in environment variable or default to 3000
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
